21eed0c217a8893d4877a02ffdb2ff8221e70f5f
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-07 22:26:02 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'sessions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
